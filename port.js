8595638c01c56cabde633a47e1c129025754bfe0
TomTimmel <tom.t.timmel@gmail.com>
2016-09-21 08:45:57 -0700

    tuesday final

--- port.js
+++ port.js
@@ -2,19 +2,19 @@ var portData = [
   {
     title: 'Memory Card Game',
     authors: 'Tom Timmel, Chris Brunner, Steven Jackson',
-    webUrl: 'http://www.alaskawildland.com',
+    webUrl: 'https://scuttling.github.io/memory-card/index.html',
     created: '2016-30-8'
   },
   {
     title: 'Salmon Cookies',
     authors: 'Tom Timmel',
-    webUrl: 'http://www.alaskawildland.com',
+    webUrl: 'https://tomtimmel.github.io/salmon_cookies/sales.html',
     created: '2016-30-8'
   },
   {
     title: 'About Me',
     authors: 'Tom Timmel',
-    webUrl: 'http://www.alaskawildland.com',
+    webUrl: 'https://tomtimmel.github.io/about_me/index.html',
     created: '2016-30-8'
   }
 ];
